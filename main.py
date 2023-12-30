bd93913d04344de70dfc3ad738f8f29a73827d06
Restart <79792966+RestartB@users.noreply.github.com>
2023-12-30 01:34:26 +0000

    Start main.py - Define hardcoded variables

---
+++ main.py
@@ -0,0 +1,17 @@
+# AI Radio Project
+# Restart - 2023
+
+# ------ Imports ------
+import pygame
+
+# ------ Hardcoded Variables ------
+indent = True
+songannounce = True
+commentary = True
+adverts = True
+musicpath = "\music"
+commentarypath = "\commentary"
+advertpath = "\advert"
+
+
+# ------ Music Handler ------
